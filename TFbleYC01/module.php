20bb600b9c714977ab516db41afa6bfef2c5dede
Thomas Frese <tf@log-in.de>
2022-08-17 16:57:18 +0200

    add cl-value
    add cl-value

--- TFbleYC01/module.php
+++ TFbleYC01/module.php
@@ -3,7 +3,6 @@ class TFbleYC01 extends IPSModule{
     
     public function Create(){
         parent::Create();
-        //$this->RegisterPropertyString("deviceTopic", "");
         $this->ConnectParent("{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}");
 
 		if (!IPS_VariableProfileExists('TFYC.ec')) {
@@ -20,26 +19,27 @@ class TFbleYC01 extends IPSModule{
 			IPS_SetVariableProfileIcon ('TFYC.tds', 'Snow');
 			IPS_SetVariableProfileText('TFYC.tds', '', ' ppm');
 			IPS_SetVariableProfileDigits('TFYC.tds', 0); 
-			IPS_SetVariableProfileValues('TFYC.tds', 0, 5000, 10);
+			IPS_SetVariableProfileValues('TFYC.tds', 0, 9990, 1);
 			IPS_SetVariableProfileAssociation('TFYC.tds', 0, 'niedrig %d', 'Warning', 0x0000FF);
 			IPS_SetVariableProfileAssociation('TFYC.tds', 250, '%d', '', 0x00FF00);
 			IPS_SetVariableProfileAssociation('TFYC.tds', 2000, 'hoch %d', 'Warning', 0xFF0000);
 		}
-        if (!IPS_VariableProfileExists('TFYC.saltTds')) {
-            IPS_CreateVariableProfile('TFYC.saltTds', 1);
-			IPS_SetVariableProfileIcon ('TFYC.saltTds', 'Snow');
-			IPS_SetVariableProfileText('TFYC.saltTds', '', ' ppm'); 
-			IPS_SetVariableProfileValues('TFYC.saltTds', 0, 10000, 10);
-			IPS_SetVariableProfileAssociation('TFYC.saltTds', 0, 'niedrig %d', 'Warning', 0x0000FF);
-			IPS_SetVariableProfileAssociation('TFYC.saltTds', 2500, '%d', '', 0x00FF00);
-			IPS_SetVariableProfileAssociation('TFYC.saltTds', 3500, 'hoch %d', 'Warning', 0xFF0000);
+        if (!IPS_VariableProfileExists('TFYC.cl')) {
+            IPS_CreateVariableProfile('TFYC.cl', 2);
+			IPS_SetVariableProfileIcon ('TFYC.cl', 'Speedo');
+			IPS_SetVariableProfileText('TFYC.cl', '', ' mg/L');
+			IPS_SetVariableProfileDigits('TFYC.cl', 1); 
+			IPS_SetVariableProfileValues('TFYC.cl', 0, 4, 0.1);
+			IPS_SetVariableProfileAssociation('TFYC.cl', 0, 'niedrig %.2f', 'Warning', 0x0000FF);
+			IPS_SetVariableProfileAssociation('TFYC.cl', 0.5, '%.2f', '', 0x00FF00);
+			IPS_SetVariableProfileAssociation('TFYC.cl', 1.3, 'hoch %.2f', 'Warning', 0xFF0000);
 		}
 
 		if (!IPS_VariableProfileExists('TFYC.ph')) {
             IPS_CreateVariableProfile('TFYC.ph', 2);
 			IPS_SetVariableProfileIcon ('TFYC.ph', 'Gauge');
 			IPS_SetVariableProfileDigits('TFYC.ph', 1); 
-			IPS_SetVariableProfileValues('TFYC.ph', 0, 14, 1);
+			IPS_SetVariableProfileValues('TFYC.ph', 0, 14, 0.1);
 			IPS_SetVariableProfileAssociation('TFYC.ph', 0, 'sauer %.2f', 'Warning', 0xFF0000);
 			IPS_SetVariableProfileAssociation('TFYC.ph', 6.8, '%.2f', '', 0x00FF00);
 			IPS_SetVariableProfileAssociation('TFYC.ph', 7.4, 'alkalisch %.2f', 'Warning', 0x0000FF);
@@ -49,8 +49,8 @@ class TFbleYC01 extends IPSModule{
             IPS_CreateVariableProfile('TFYC.orp', 1);
 			IPS_SetVariableProfileIcon ('TFYC.orp', 'DoctorBag');
 			IPS_SetVariableProfileText('TFYC.orp', '', ' mV');
-			IPS_SetVariableProfileValues('TFYC.orp', 0, 5000, 10);
-			IPS_SetVariableProfileAssociation('TFYC.orp', 0, 'niedrig %d', 'Warning', 0x0000FF);
+			IPS_SetVariableProfileValues('TFYC.orp', -1200, 1200, 1);
+			IPS_SetVariableProfileAssociation('TFYC.orp', -1200, 'niedrig %d', 'Warning', 0x0000FF);
 			IPS_SetVariableProfileAssociation('TFYC.orp', 650, '%d', '', 0x00FF00);
 			IPS_SetVariableProfileAssociation('TFYC.orp', 750, 'hoch %d', 'Warning', 0xFF0000);
 		}
@@ -70,14 +70,14 @@ class TFbleYC01 extends IPSModule{
     public function ApplyChanges(){
         parent::ApplyChanges();
 		$ph_ID			= $this->RegisterVariableFloat("ph", "pH", "TFYC.ph", 1);
-		$ec_ID			= $this->RegisterVariableInteger("ec", "Leitwert (EC)", "TFYC.ec", 2);
-		$tds_ID			= $this->RegisterVariableInteger("tds", "Feststoffe (TDS)", "TFYC.tds", 3);
-		$saltTds_ID		= $this->RegisterVariableInteger("saltTds", "Salzgehalt", "TFYC.saltTds", 4);
+		$cl_ID			= $this->RegisterVariableFloat("cl", "Chlorgehalt", "TFYC.cl", 2);
+		$ec_ID			= $this->RegisterVariableInteger("ec", "Leitwert (EC)", "TFYC.ec", 3);
+		$tds_ID			= $this->RegisterVariableInteger("tds", "Feststoffe (TDS)", "TFYC.tds", 4);
 		$orp_ID			= $this->RegisterVariableInteger("orp", "Redoxpotenzial", "TFYC.orp", 5);
 		$temp_ID		= $this->RegisterVariableFloat("temp", "Temperatur", "~Temperature", 6);
 		$batt_ID		= $this->RegisterVariableInteger("batt", "Batterie", "~Battery.100", 7);
 		$backlight_ID	= $this->RegisterVariableBoolean("backlight", "Display-Beleuchtung", "~Switch", 8);
-		$holdReading_ID	= $this->RegisterVariableBoolean("holdReading", "Lesen angehalten", "~Switch", 9);
+		$holdReading_ID	= $this->RegisterVariableBoolean("holdReading", "Lesen anhalten", "~Switch", 9);
 		$getData_ID		= $this->RegisterVariableBoolean("getData", "Daten abrufen", "~Switch", 10);
 		$lastData_ID	= $this->RegisterVariableInteger("lastData", "Letzter Abruf", "~UnixTimestamp", 11);
 
@@ -152,9 +152,9 @@ class TFbleYC01 extends IPSModule{
 					case "bleYc01Data" 		:
 						$valueData["ec"] != $this->GetValue("ec") ? $this->SetValue("ec", $valueData["ec"]) : 1;
 						$valueData["tds"] != $this->GetValue("tds") ? $this->SetValue("tds", $valueData["tds"]) : 1;
-						$valueData["saltTds"] != $this->GetValue("saltTds") ? $this->SetValue("saltTds", $valueData["saltTds"]) : 1;
 						$valueData["ph"] != $this->GetValue("ph") ? $this->SetValue("ph", $valueData["ph"]) : 1;
 						$valueData["orp"] != $this->GetValue("orp") ? $this->SetValue("orp", $valueData["orp"]) : 1;
+						$valueData["cl"] != $this->GetValue("cl") ? $this->SetValue("cl", $valueData["cl"]) : 1;
 						$valueData["temp"] != $this->GetValue("temp") ? $this->SetValue("temp", $valueData["temp"]) : 1;
 						$valueData["battery"] != $this->GetValue("batt") ? $this->SetValue("batt", $valueData["battery"]) : 1;
 						$valueData["holdReading"] != $this->GetValue("holdReading") ? $this->SetValue("holdReading", $valueData["holdReading"]) : 1;
